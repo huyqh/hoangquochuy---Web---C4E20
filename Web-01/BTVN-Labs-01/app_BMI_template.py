0edb4ecc7c68143388b673c45b9d812a0de3dda4
huyqh <huy.br31hn@gmail.com>
2018-08-17 12:55:11 +0700

    intial commit

---
+++ Web-01/BTVN-Labs-01/app_BMI_template.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route("/BMI_template/<int:weight>/<int:high>")
+def BMI_template(weight, high):
+    BMI = (weight * 10000) / (high * high)
+    return render_template("BMI_index.html", BMI = BMI)
+
+if __name__ == "__main__":
+  app.run(port=8000, debug=True)
+
+
+
+
